e34145ee175f6142ac1ada1cf7cb7bbe579e4187
ProcessMonitor <zhang19970321@qq.com>
2024-11-17 23:22:53 +0800

    lambdaTest 和 函数式编程接口

--- base-test/src/test/java/LambdaSystem.java
+++ base-test/src/test/java/LambdaSystem.java
@@ -1,3 +1,37 @@
+import com.google.common.base.Supplier;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LambdaSystem {
 
+    @FunctionalInterface
+    interface LambdaType1 {
+        boolean operate(int a, int b);
+    }
+
+    @FunctionalInterface
+    interface LambdaType2<T> {
+        T operate();
+    }
+
+    @FunctionalInterface
+    interface LambdaType3<T , R> {
+        R operate(T input);
+    }
+
+    public static void main(String[] args) {
+        LambdaType1 lambdaType1 = (int a, int b) -> a == b;
+        LambdaType2<List<Object>> arrayListLambdaType2 = () -> new ArrayList<>();
+        LambdaType2<String[]> type2 = () -> new String[]{};
+//        Supplier<String[]> type21 = () -> new String[]{};
+
+        LambdaType3<Integer, String> integerStringLambdaType3 = (Integer a) -> a.toString();
+        // 等价于：
+        LambdaType3<Integer, String> integerStringLambdaType4 = (a) -> a.toString();
+
+
+        ArrayList<Object> objects = new ArrayList<>();
+//        objects.stream().filter(lambdaType1::operate).collect()
+    }
 }
